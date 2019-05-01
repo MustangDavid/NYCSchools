cab7b4d409d846a713ea1bb487ffa96d548a5c0e
David <david.maiman@gmail.com>
2019-04-30 21:54:22 -0400

    added stuff

---
+++ 20190430-DavidMaiman-NYCSchools/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20190430-DavidMaiman-NYCSchools
+//
+//  Created by DAVID MAIMAN on 4/30/19.
+//  Copyright © 2019 DAVID MAIMAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
